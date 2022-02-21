c7efbd4c39593f5697f901d973547487b9b0a1b1
djthap <dhananjaythap111@gmail.com>
2022-02-22 00:11:25 +0530

    push/[0/

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-<script src="https://kit.fontawesome.com/65d8f8c3d1.js" crossorigin="anonymous"></script>
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
